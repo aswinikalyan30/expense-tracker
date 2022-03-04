70aeabfb6ed879803768e65e1ef4fdcb4852c464
Abajit Dey <abajitdeyag10118@gmail.com>
2022-03-04 22:15:41 +0530

    added the div to show balance and other details, added a header to greet the user with their names

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -1,18 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import "./../scss/Basic.scss";
 
 const Basic = (props) => {
-    const { balance } = props;
+  const { balance } = props;
   return (
     <main>
       <h3>
         Hello <span id="user-name">Basically</span>
       </h3>
       <section id="basic-info">
-          <span id="balance-text">Here's your remaining balance</span>
-          <span id="balance-amount">{balance}</span>
-          <span>You've spent $520 this week out of $725.18</span>
+        <span id="balance-text">Here's your remaining balance</span>
+        <span id="balance-amount">
+          {balance}
+          <span id="currency">INR</span>
+        </span>
+        <p id="extra-text">
+          You've spent {String.fromCharCode(8377)}520 this week.
+          That's 30% more since last week.
+        </p>
       </section>
     </main>
   );
